34b4d41c84610b591ddba1ec39825c0a3326acd8
jhon landeo <jhon.landeo@alarcongroup.us>
2026-06-15 20:28:04 -0500

    feat: initialize new Nuxt project with Pinia integration and path aliases

---
+++ frontend-vue/nuxt.config.ts
@@ -0,0 +1,8 @@
+export default defineNuxtConfig({
+  srcDir: 'src/',
+  modules: ['@pinia/nuxt'],
+  devtools: { enabled: true },
+  alias: {
+    '@': '/home/jhon/Documents/work-projects/odiseo-new/frontend-vue/src'
+  }
+})
